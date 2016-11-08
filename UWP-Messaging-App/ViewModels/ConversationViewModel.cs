563f14af1cb5bde51d36628ed4faa73fcfd68612
Ross Byrne <ross.byrne@outlook.ie>
2016-11-08 15:56:04 +0000

    More work on the conversation view model

--- UWP-Messaging-App/ViewModels/ConversationViewModel.cs
+++ UWP-Messaging-App/ViewModels/ConversationViewModel.cs
@@ -3,12 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UWP_Messaging_App.Data;
 
 namespace UWP_Messaging_App.ViewModels
 {
     // model to handle conversations in app
     class ConversationViewModel
     {
+
+        private ConversationService convoService;
+        private Conversation conversation;
+
+        public ConversationViewModel(string id)
+        {
+            this.conversation = convoService.getConversationByID(id);
+
+        } // Constructor()
+
+        public Conversation getConversation()
+        {
+            return conversation;
+        }
+
+        public List<Message> getMessages()
+        {
+            return conversation.messages;
+        }
+
+        // add message to conversation
+        public void sendMessage(string senderId, string recipientId, string message)
+        {
+            Message m = new Message();
+            m.id = Guid.NewGuid().ToString();
+            m.senderId = senderId;
+            m.recipientId = recipientId;
+            m.message = message;
+
+            conversation.messages.Add(m);
+
+        } // sendMessage()
 
     } // class
 
