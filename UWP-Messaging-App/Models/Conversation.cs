528d8dd1f533d87cd265f09e522b42b817c507ad
Ross Byrne <ross.byrne@outlook.ie>
2016-11-08 15:12:37 +0000

    Added Conversations class, updated frontend

---
+++ UWP-Messaging-App/Models/Conversation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_Messaging_App.Models
+{
+    // class to hold the data for a conversation
+    class Conversation
+    {
+        public string id { get; set; }                  // id of conversation
+        public List<string> userIds { get; set; }       // list of ids for users involved in conversation
+        public List<Message> messages { get; set; }     // list of all messages in conversation
+
+    } // class
+
+} // namespace
