927597a1b43200b6ba9a939bc2f54a4bd76a9376
Ross Byrne <ross.byrne@outlook.ie>
2016-11-08 15:06:22 +0000

    Added message class and listview to display messages

---
+++ UWP-Messaging-App/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_Messaging_App.Models
+{
+    // class for holding message data
+    class Message
+    {
+        public string id { get; set; }              // id of message
+        public string message { get; set; }         // the message text
+        public string senderId { get; set; }        // id of user who sent message
+        public string recipientId { get; set; }     // id of user receiving message
+
+    } // class
+
+} // namespace
