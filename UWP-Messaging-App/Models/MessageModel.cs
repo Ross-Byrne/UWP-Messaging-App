e8f947e43e6711e511ddaf504b2183097f009ac5
Ross Byrne <ross.byrne@outlook.ie>
2016-11-29 15:10:47 +0000

    Added Model and view model for message

---
+++ UWP-Messaging-App/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_Messaging_App.Models
+{
+    // model for Message class.
+    // Methods for saving and getting messages from couchDB will be here
+    class MessageModel
+    {
+    }
+}
