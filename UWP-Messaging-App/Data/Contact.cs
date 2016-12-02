d621fdc11740f547b55c105feb3cf42c9999a5ec
Ross Byrne <ross.byrne@outlook.ie>
2016-12-02 17:48:36 +0000

    Added Contacts class

---
+++ UWP-Messaging-App/Data/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_Messaging_App.Data
+{
+    // class to hold information about indevidual contacts
+    class Contact
+    {
+        public string ContactId { get; set; }
+        public string ConversationId { get; set; }
+        public string Name { get; set; }
+    } // class
+}
