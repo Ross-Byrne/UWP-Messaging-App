91d025b540d3eb70a73070081679289b814afb16
Ross Byrne <ross.byrne@outlook.ie>
2016-12-02 18:05:10 +0000

    Setting contacts in MVVM pattern

---
+++ UWP-Messaging-App/Models/ContactModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWP_Messaging_App.Data;
+
+namespace UWP_Messaging_App.Models
+{
+    class ContactModel
+    {
+        private Contact contact = new Contact();
+
+        public string ContactId
+        {
+            get { return contact.ContactId; }
+            set { contact.ContactId = value; }
+        }
+
+        public string ConversationId
+        {
+            get { return contact.ConversationId; }
+            set { contact.ConversationId = value; }
+        }
+
+        public string Name
+        {
+            get { return contact.Name; }
+            set { contact.Name = value; }
+        }
+    }
+}
---
+++ UWP-Messaging-App/ViewModels/ContactViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWP_Messaging_App.Data;
+using UWP_Messaging_App.Models;
+
+namespace UWP_Messaging_App.ViewModels
+{
+    // view model for contact
+    class ContactViewModel : NotificationBase<Contact>
+    {
+
+        public ContactViewModel(Contact contact = null) : base(contact) { }
+
+        public string ContactId
+        {
+            get { return This.ContactId; }
+            set { SetProperty(This.ContactId, value, () => This.ContactId = value); }
+        }
+
+        public string ConversationId
+        {
+            get { return This.ConversationId; }
+            set { SetProperty(This.ConversationId, value, () => This.ConversationId = value); }
+        }
+
+        public string Name
+        {
+            get { return This.Name; }
+            set { SetProperty(This.Name, value, () => This.Name = value); }
+        }
+
+    } // class
+}
